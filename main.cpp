92beaa144b0e433f7853e817ba07af4746ac23b7
Danil <nerdylivefeed@gmail.com>
2018-01-23 23:50:34 +0300

    Release 1.1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "\tПрямоугольный треугольник в С++\n";
+	cout << endl;
+	cout << "\t*\n";
+	cout << "\t**\n";
+	cout << "\t***\n";
+	cout << "\t****\n";
+	cout << "\t*****\n";
+	cout << "\t******\n";
+	cout << "\t*******\n";
+	cout << endl;
+	return 0;
+}
